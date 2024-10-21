f57c4020a9cd9f632d5d5f6ee1e7b42789721f5c
TharuHiru <hirusha-im21011@stu.kln.ac.lk>
2024-10-21 23:20:14 +0530

    Added on pressed method to buttons

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/calculatorLogic.dart';
 
 class Buttons extends StatelessWidget {
   // Numbers
@@ -31,17 +32,19 @@ class Buttons extends StatelessWidget {
   static const String delete = "del";
   static const String reset = "C";
   const Buttons(
-      {super.key, required this.label, this.textColor = Colors.white});
+      {super.key,
+      required this.label,
+      required this.onPressed,
+      this.textColor = Colors.white});
 
   final String label;
   final Color textColor;
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {
-        // Handle button tap
-      },
+      onTap: onPressed,
       child: Material(
         // material button
         elevation: 3,
@@ -64,69 +67,96 @@ class Buttons extends StatelessWidget {
   }
 
   static List<Widget> row1() => [
-        const Buttons(
-          label: reset,
-          textColor: Color.fromARGB(255, 164, 62, 54),
-        ),
-        const Buttons(
-          label: openBr,
-          textColor: Color.fromARGB(255, 48, 110, 160),
-        ),
-        const Buttons(
-          label: closeBr,
-          textColor: Color.fromARGB(255, 48, 110, 160),
-        ),
-        const Buttons(
-          label: mul,
-        ),
+        Buttons(
+            label: reset,
+            textColor: const Color.fromARGB(255, 164, 62, 54),
+            onPressed: () => Calculatorlogic.onButtonPressed(reset)),
+        Buttons(
+            label: openBr,
+            textColor: const Color.fromARGB(255, 48, 110, 160),
+            onPressed: () => Calculatorlogic.onButtonPressed(openBr)),
+        Buttons(
+            label: closeBr,
+            textColor: const Color.fromARGB(255, 48, 110, 160),
+            onPressed: () => Calculatorlogic.onButtonPressed(closeBr)),
+        Buttons(
+            label: mul,
+            textColor: const Color.fromARGB(255, 48, 110, 160),
+            onPressed: () => Calculatorlogic.onButtonPressed(mul)),
       ];
 
   static List<Widget> row2() => [
-        const Buttons(
-          label: sign,
-          textColor: Color.fromARGB(255, 48, 110, 160),
-        ),
-        const Buttons(
-          label: percentage,
-          textColor: Color.fromARGB(255, 48, 110, 160),
-        ),
-        const Buttons(
-          label: sqrt,
-          textColor: Color.fromARGB(255, 48, 110, 160),
-        ),
-        const Buttons(
-          label: div,
-        ),
+        Buttons(
+            label: sign,
+            textColor: const Color.fromARGB(255, 48, 110, 160),
+            onPressed: () => Calculatorlogic.onButtonPressed(sign)),
+        Buttons(
+            label: percentage,
+            textColor: const Color.fromARGB(255, 48, 110, 160),
+            onPressed: () => Calculatorlogic.onButtonPressed(percentage)),
+        Buttons(
+            label: sqrt,
+            textColor: const Color.fromARGB(255, 48, 110, 160),
+            onPressed: () => Calculatorlogic.onButtonPressed(sqrt)),
+        Buttons(
+            label: div,
+            textColor: const Color.fromARGB(255, 48, 110, 160),
+            onPressed: () => Calculatorlogic.onButtonPressed(div)),
       ];
 
   static List<Widget> row3() => [
-        const Buttons(label: num7),
-        const Buttons(label: num8),
-        const Buttons(label: num9),
-        const Buttons(
-          label: sub,
-        ),
+        Buttons(
+            label: num7,
+            onPressed: () => Calculatorlogic.onButtonPressed(num7)),
+        Buttons(
+            label: num8,
+            onPressed: () => Calculatorlogic.onButtonPressed(num8)),
+        Buttons(
+            label: num9,
+            onPressed: () => Calculatorlogic.onButtonPressed(num9)),
+        Buttons(
+            label: sub,
+            textColor: const Color.fromARGB(255, 48, 110, 160),
+            onPressed: () => Calculatorlogic.onButtonPressed(sub)),
       ];
 
   static List<Widget> row4() => [
-        const Buttons(label: num4),
-        const Buttons(label: num5),
-        const Buttons(label: num6),
-        const Buttons(
-          label: add,
-        ),
+        Buttons(
+            label: num4,
+            onPressed: () => Calculatorlogic.onButtonPressed(num4)),
+        Buttons(
+            label: num5,
+            onPressed: () => Calculatorlogic.onButtonPressed(num5)),
+        Buttons(
+            label: num6,
+            onPressed: () => Calculatorlogic.onButtonPressed(num6)),
+        Buttons(
+            label: add,
+            textColor: const Color.fromARGB(255, 48, 110, 160),
+            onPressed: () => Calculatorlogic.onButtonPressed(Buttons.reset)),
       ];
 
   static List<Widget> row5() => [
-        const Buttons(label: num1),
-        const Buttons(label: num2),
-        const Buttons(label: num3),
+        Buttons(
+            label: num1,
+            onPressed: () => Calculatorlogic.onButtonPressed(num1)),
+        Buttons(
+            label: num2,
+            onPressed: () => Calculatorlogic.onButtonPressed(num2)),
+        Buttons(
+            label: num3,
+            onPressed: () => Calculatorlogic.onButtonPressed(num3)),
       ];
 
   static List<Widget> row6() => [
-        const Buttons(label: num0),
-        const Buttons(label: dot),
-        const Buttons(
-            label: delete, textColor: Color.fromARGB(255, 164, 62, 54)),
+        Buttons(
+            label: num0,
+            onPressed: () => Calculatorlogic.onButtonPressed(num0)),
+        Buttons(
+            label: dot, onPressed: () => Calculatorlogic.onButtonPressed(dot)),
+        Buttons(
+            label: delete,
+            textColor: const Color.fromARGB(255, 164, 62, 54),
+            onPressed: () => Calculatorlogic.onButtonPressed(delete)),
       ];
 }
---
+++ lib/calculatorLogic.dart
@@ -0,0 +1,7 @@
+class Calculatorlogic {
+  String display = '0';
+  String result = '0';
+  String operator = '0';
+
+  static void onButtonPressed(String label) {}
+}
